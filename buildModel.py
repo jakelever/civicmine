dff176bd403261cb8f17d5bb725b476154cc3264
Jake Lever <jake.lever@gmail.com>
2018-01-23 17:37:34 -0500

    Build specific models for different relation types

--- buildModel.py
+++ buildModel.py
@@ -14,8 +14,15 @@ if __name__ == '__main__':
 
 	thresholds = {'Driver':0.89, 'Oncogene': 0.94, 'Tumor_Suppressor': 0.97}
 
+	relationInfo = []
+	relationInfo.append(('Diagnostic','Diagnostic',('cancer','gene')))
+	relationInfo.append(('Predictive/Prognostic','Prognostic',('cancer','gene')))
+	relationInfo.append(('Predictive/Prognostic','Predictive',('cancer','drug','gene')))
+	relationInfo.append(('Predisposing','Predisposing',('cancer','gene')))
+
 	#for relationType,outModel in zip(['Driver','Oncogene','Tumor_Suppressor'], [args.outModel_Driver,args.outModel_Oncogene,args.outModel_TumorSuppressor] ):
-	for relationType in ['Diagnostic','Predictive/Prognostic','Predisposing']:
+	#for relationType in ['Diagnostic','Predictive/Prognostic','Predisposing']:
+	for relationType,replacementType,entityTypes in relationInfo:
 		print("Building %s model" % relationType)
 		print("  Loading training")
 		goldDir = 'gold'
@@ -23,15 +30,18 @@ if __name__ == '__main__':
 	
 		for doc in trainCorpus.documents:
 			doc.relations = [ r for r in doc.relations if r.relationType == relationType ]
+			doc.relations = [ r for r in doc.relations if len(r.entityIDs) == len(entityTypes) ]
+			for r in doc.relations:
+				r.relationType = replacementType
 
 		print("  Doing training")
 		features = "entityTypes,unigramsBetweenEntities,bigrams,dependencyPathEdges,dependencyPathEdgesNearEntities".split(',')
 		threshold = 0.7 #thresholds[relationType]
-		classifier = kindred.RelationClassifier(classifierType='LogisticRegression',threshold=threshold,features=features,acceptedEntityTypes=[('cancer','gene')])
+		classifier = kindred.RelationClassifier(classifierType='LogisticRegression',threshold=threshold,features=features,entityCount=len(entityTypes),acceptedEntityTypes=[entityTypes])
 		classifier.train(trainCorpus)
 
 		print("  Saving classifer")
-		outModel = os.path.join('models',"%s.model" % relationType.replace('/','_'))
+		outModel = os.path.join('models',"%s.model" % replacementType)
 		with open(outModel,'wb') as f:
 			pickle.dump(classifier,f)
 
